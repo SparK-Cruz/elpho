48a62d757417a489dbf1033d8f35a1ecfb4e6243
Rodrigo Nery <rodrigonery@hotmail.com.br>
2011-11-16 19:42:20 -0200

    Update Starter.php

--- Starter.php
+++ Starter.php
@@ -13,37 +13,44 @@
 			self::registerMain();
 			register_shutdown_function(array(Starter,"callPrimaryMethods"));
 		}
+		
 		private static function registerEntry($method){
 			self::$entryMethod = $method;
 		}
+		
 		private static function registerExit($method){
 			self::$exitMethod = $method;
 		}
+		
 		private static function callEntry($args=array()){
-			if(!self::$entryMethod) return;
+			(!self::$entryMethod)? return;
 			call_user_func(self::$entryMethod,$args);
 		}
+		
 		private static function callExit(){
-			if(!self::$exitMethod) return;
+			(!self::$exitMethod)? return;
 			call_user_func(self::$exitMethod);
 		}
+		
 		private static function registerEntryClass($target){
 			$entry = array($target,"main");
 			$exit = array($target,"cleanUp");
 			
-			if(method_exists($entry[0],$entry[1]))
+			(method_exists($entry[0],$entry[1]))?
 				self::registerEntry($entry);
 			
-			if(method_exists($exit[0],$exit[1]))
+			(method_exists($exit[0],$exit[1]))?
 				self::registerExit($exit);
 		}
+		
 		public static function callPrimaryMethods(){
 			chdir(dirname($_SERVER["SCRIPT_FILENAME"]));
 			self::callEntry($_REQUEST);
 			self::callExit();
 		}
+		
 		public static function registerMain($filename=null){
-			if(!$filename) $filename = $_SERVER["SCRIPT_FILENAME"];
+			(!$filename)? $filename = $_SERVER["SCRIPT_FILENAME"];
 			$currentClass = basename($filename,".php");
 			self::registerEntryClass($currentClass);
 		}
