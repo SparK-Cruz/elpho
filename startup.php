d4271428988aafdf24cbff6305f8fdb4f3a45d4a
SparK Cruz <spark@limao.com.br>
2012-01-13 14:55:32 -0200

    removido comentários inúteis

--- startup.php
+++ startup.php
@@ -1,10 +1,4 @@
 <?php
-	/**
-	 * Arquivo de inicialização do Elpho framework
-	 * Elpho = Extensão Lógica de PHP OO
-	 * 
-	 * v0.2
-	 */
 	if(defined("DEBUG")){
 		error_reporting(E_ALL);
 		ini_set('display_errors', '1');
